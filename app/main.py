7cf97bbffb537766efc6a8604e0cac0396c9e373
Utsho MH <utshomh.dev@gmail.com>
2026-09-14 21:56:15 +0600

    add image upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI(
     title="CardLens API",
     version="0.0.1"
 )
 
+UPLOAD_DIR = "upload"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 @app.get('/')
 def home():
     return {
         "message": "CardLens API is running"
+    }
+
+@app.post('/upload')
+async def upload_card(
+    file: UploadFile = File(...)
+):
+    file_path = os.path.join(
+        UPLOAD_DIR,
+        file.filename
+    )
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(
+            file.file,
+            buffer
+        )
+
+    return {
+        "filename": file.filename,
+        "status": "uploaded"
     }
